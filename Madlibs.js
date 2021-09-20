08dff9805091b864626a4eb5d92b9079c192d3ad
Jenny <jennyd.moncion@gmail.com>
2021-09-19 22:19:43 -0400

    first commit

---
+++ Madlibs.js
@@ -0,0 +1,39 @@
+function madlibs() {
+    const player = prompt("Would you like storyOne or storyTwo? ");
+
+    if (player => storyOne ) {
+    let adjective = prompt("Type an adjective: ");
+    let place = prompt("Type a general place: ");
+    let name1 = prompt("Type a vehicle name in plural: ");
+    let noun = prompt("Type a plural noun: ");
+    let verb1 = prompt("Type a ing verb: ");
+    let verb2 = prompt("Type another ing verb: ");
+    let name2 = prompt("Type a food name: ");
+    let name3 = prompt("Type the name of a sport: ");
+    let verb3 = prompt("Type another ing verb: ");
+    let emotion = prompt("Type an emotion: ");
+    let number = prompt("Type a number: ");
+    
+    { return storyOne = '"A vacation is when you take a trip to some' + adjective + 'place near an amazing' + place + '. A good vacation place is one where you can ride' + name1 + 'or play' + noun + 'or go hunting for' + verb1 + '. I like to spend my time' + verb1 + 'or' + verb2 + '. When parents go on a vacation, they spend their time eating three' + name2 + 'a day. Usually, fathers play' + name3 + ', and mothers spend their time' + verb3 + '. Adults need vacations more than kids because adults are always very' + emotion + 'because they have to work' + number + 'hours every day all year just to afford their vacations!"';}
+    }
+    else {
+    let adjective = prompt("Type an adjective: ");
+    let place = prompt("Type a famous place: ");
+    let noun = prompt("Type a plural noun: ");
+    let adjective2 = prompt("Type another adjective: ");
+    let food = prompt("Type a food (plural): ");
+    let emotion = prompt("Type an emotion: ");
+    let vern = prompt("Type an 'ing' verb: ");
+    let place = prompt("Type a common place: ");
+    let adjective3 = prompt("Type another adjective: ");
+    let noun2 = prompt("Type a noun: ");
+    let adjective4 = prompt("Type another adjective: ");
+    let noun3 = prompt("Type another noun: ");
+    let family = prompt("Type a family member: ");
+    let adjective5 = prompt("Type another adjective: ");
+
+   { return storyTwo = `Today we took a {} fieldtrip to {}. They're famous for making {} and for cooking {} {}. Eating all that food made me feel {}. Next we enjoyed the local tradition of {} in the middle of the {}! Finally, we went shopping for souvenirs. I bought a {} {} for myself, and a {} {} for my favorite {}. I'll definitely never forget this {} trip!`}
+    }
+};
+
+madlibs();
